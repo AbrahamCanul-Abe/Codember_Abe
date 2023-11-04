a95d18151f5e6e19de7846d9887fbcec23ede4a2
Jesus Abraham Canul Couoh <Jesus.Canul@soltum.com.mx>
2023-11-03 23:09:00 -0600

    Agregar archivos de proyecto.

---
+++ Codember_Abe/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string mensaje = "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon";
+        Dictionary<string, int> contadorPalabras = ContarPalabras(mensaje);
+        string resultado = GenerarResultado(contadorPalabras, mensaje);
+        Console.WriteLine(resultado);
+    }
+
+    static Dictionary<string, int> ContarPalabras(string mensaje)
+    {
+        string[] palabras = mensaje.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        Dictionary<string, int> contadorPalabras = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (string palabra in palabras)
+        {
+            string palabraNormalizada = palabra.ToLower();
+            if (contadorPalabras.ContainsKey(palabraNormalizada))
+            {
+                contadorPalabras[palabraNormalizada]++;
+            }
+            else
+            {
+                contadorPalabras[palabraNormalizada] = 1;
+            }
+        }
+
+        return contadorPalabras;
+    }
+
+    static string GenerarResultado(Dictionary<string, int> contadorPalabras, string mensaje)
+    {
+        List<string> resultado = new List<string>();
+        string[] palabras = mensaje.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string palabra in palabras)
+        {
+            string palabraNormalizada = palabra.ToLower();
+            if (contadorPalabras.ContainsKey(palabraNormalizada))
+            {
+                resultado.Add(palabraNormalizada + contadorPalabras[palabraNormalizada]);
+                contadorPalabras.Remove(palabraNormalizada);
+            }
+        }
+
+        return string.Join("", resultado);
+    }
+}
